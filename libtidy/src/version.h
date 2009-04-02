67e80686ec729b15cd9b51099b98ec74572c45d9
Andy Lester <andy@petdance.com>
2009-04-02 01:33:03 -0500

    brought in tidy

---
+++ libtidy/src/version.h
@@ -0,0 +1,14 @@
+/* version information
+
+  (c) 2007-2008 (W3C) MIT, ERCIM, Keio University
+  See tidy.h for the copyright notice.
+
+  CVS Info :
+
+    $Author: arnaud02 $ 
+    $Date: 2008/03/22 20:52:09 $ 
+    $Revision: 1.41 $ 
+
+*/
+
+static const char TY_(release_date)[] = "22 March 2008";
